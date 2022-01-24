20c4c81827dee9761125aa3b628893ba274071b5
Shlok gupta <shlokcoder12@gmail.com>
2022-01-24 12:20:23 +0530

    Add files via upload

---
+++ CameraPush.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPush : MonoBehaviour
+{
+
+void Start()
+{
+	
+}
+
+void Update()
+{
+	gameObject.transform.position = Vector3.forward * Time.deltaTime * Speed;
+}
+}
